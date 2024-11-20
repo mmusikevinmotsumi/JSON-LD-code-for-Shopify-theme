4a60e9a7fc3dd302136174182de8230ee78bdbc0
mmusikevinmotsumi <mmusikevinmotsumi@gmail.com>
2024-11-20 01:24:05 -0500

    Create JSON-LD after update.js

---
+++ JSON-LD after update.js
@@ -0,0 +1,29 @@
+<script type="application/ld+json">
+{
+  "@context": "http://schema.org/",
+  "@id": "https://jewyear.com/products/happy-jew-year-menorah#product",
+  "@type": "Product",
+  "brand": {
+    "@type": "Brand",
+    "name": "Jewyear.com"
+  },
+  "category": "Seasonal & Holiday Decorations",
+  "description": "Limited edition, handmade designer menorah to celebrate the overlap of Hanukkah and New Year's Eve in both 2024 and 2027. The modern yet whimsical design features a mosaic New Year's ball that actually spins. Nickel-plated aluminum is heat resistant, easy to clean, and has a beautiful shine.\n10.75in H x 13.7in WHolds Hanukkah candles up to 3/8\" diameter\nThis order will deliver in December.Order by October 15 to insure delivery in time for Hanukkah.\n&nbsp;\n",
+  "gtin": "198715023473",
+  "hasMerchantReturnPolicy": {
+    "@type": "MerchantReturnPolicy",
+    "merchantReturnLink": "https://jewyear.com/policies/refund-policy"
+  },
+  "image": "https://jewyear.com/cdn/shop/files/Happy_Jew_Year_Menorah.png?v=1727382877&width=1920",
+  "name": "Happy Jew Year! Menorah - Celebrate Hanukkah and New Year 2024",
+  "offers": {
+    "@type": "Offer",
+    "availability": "http://schema.org/InStock",
+    "price": "54.00",
+    "priceCurrency": "USD",
+    "url": "https://jewyear.com/products/happy-jew-year-menorah?variant=49901882835233"
+  },
+  "sku": "HJY1",
+  "url": "https://jewyear.com/products/happy-jew-year-menorah"
+}
+</script>
